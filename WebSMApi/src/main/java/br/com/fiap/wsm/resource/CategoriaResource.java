c3553c4ce8e1923b096da7827c638e8c09e873c3
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-10-21 19:32:02 -0300

    CategoriaResource: Adicionado o Path da classe, atributo e construtor

--- WebSMApi/src/main/java/br/com/fiap/wsm/resource/CategoriaResource.java
+++ WebSMApi/src/main/java/br/com/fiap/wsm/resource/CategoriaResource.java
@@ -1,5 +1,16 @@
 package br.com.fiap.wsm.resource;
 
-public class CategoriaResource {
+import java.sql.SQLException;
 
+import br.com.fiap.wsm.service.CategoriaService;
+import jakarta.ws.rs.Path;
+
+@Path("/categoria")
+public class CategoriaResource {
+	// Atributo
+	private CategoriaService service;
+	//Construtor
+	public CategoriaResource() throws ClassNotFoundException, SQLException {
+		service = new CategoriaService();
+	}
 }//CLASS
