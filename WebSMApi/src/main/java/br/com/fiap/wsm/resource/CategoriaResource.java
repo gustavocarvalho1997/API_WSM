7382bc63efcb7e9c4a1e5e6799be5d795f8218f1
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-10-21 20:26:21 -0300

    CategoriaResource: Adicionado o médoto busca

--- WebSMApi/src/main/java/br/com/fiap/wsm/resource/CategoriaResource.java
+++ WebSMApi/src/main/java/br/com/fiap/wsm/resource/CategoriaResource.java
@@ -3,16 +3,19 @@ package br.com.fiap.wsm.resource;
 import java.sql.SQLException;
 import java.util.List;
 
+import br.com.fiap.wsm.exception.IdNotFoundException;
 import br.com.fiap.wsm.model.Categoria;
 import br.com.fiap.wsm.service.CategoriaService;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 import jakarta.ws.rs.core.UriBuilder;
 import jakarta.ws.rs.core.UriInfo;
 
@@ -41,4 +44,16 @@ public class CategoriaResource {
 	public List<Categoria> lista() throws SQLException {
 		return service.listar();
 	}//Listar FIM
+	
+	//busca INICIO
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response busca(@PathParam("id") int id) throws SQLException {
+		try {
+			return Response.ok(service.pesquisarPorId(id)).build();
+		} catch (IdNotFoundException e) {
+			return Response.status(Status.NOT_FOUND).build();
+		}
+	}//busca FIM
 }//CLASS
