143e9f30b2c591e12f6db445a604ee6496fcb04c
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-10-21 18:23:48 -0300

    Finalizado o modelo Categoria

--- WebSMApi/src/main/java/br/com/fiap/wsm/model/Categoria.java
+++ WebSMApi/src/main/java/br/com/fiap/wsm/model/Categoria.java
@@ -1,5 +1,22 @@
 package br.com.fiap.wsm.model;
 
-public class Categoria {
+import lombok.Data;
 
+@SuppressWarnings("unused")
+@Data
+public class Categoria {
+	
+	private int id;
+	private String nome;
+	private boolean perecivel;
+	public Categoria(int id, String nome, boolean perecivel) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.perecivel = perecivel;
+	}
+	public Categoria() {
+		super();
+	}
+	
 }
